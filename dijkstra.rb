079631ce7782cc898b49622a57416af4ef1e0cc0
nathanielyearwood <nathaniel.yearwood@uoit.net>
2016-12-10 23:58:56 -0500

    Added nodes class and tested

--- dijkstra.rb
+++ dijkstra.rb
@@ -2,6 +2,40 @@
 # CSCI3055U Final - Dijkstra's Algorithm
 
 
+# nodes of graph
+class Node
+
+	def initialize (name)
+		@name = name
+		@edges = []
+	end
+
+	# adds edge to this node
+	def addEdge (node, dist)
+		@edges.push([node, dist])
+		node.addOtherEdge(self, dist)
+	end
+
+	# used to add edge to other node when called
+	def addOtherEdge (node, dist)
+		@edges.push([node, dist])
+	end
+
+	def to_s
+		'Node: '+ @name + '      Edges: ' + @edges.length.to_s
+	end
+
+end
+
+#testing nodes
+a = Node.new('a')
+b = Node.new('b')
+a.addEdge(b, 5)
+puts a
+puts b
+
+
+
 test = [5, 7, 3, 1, 9, 4]
 def sort (list) #function to sort list of given numbers
 	list.each do
@@ -16,4 +50,4 @@ end
 
 test = sort(test)
 
-puts test
+# puts test
